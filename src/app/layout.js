e201e3de0115c1da14b91e1762f0a14bd487efb8
Naheun Alam <naheunpranto258@gmail.com>
2026-06-19 19:16:38 +0600

    Recap: Data Fetching Flow in Next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,15 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme="dark"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header/>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+ 
+
+const PostsPage = () => {
+    return (
+        <div>
+            <h2>Posts are coming soon!!!!!</h2>
+        </div>
+    );
+};
+
+export default PostsPage;
