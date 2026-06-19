e7d6c66c74a8c9245eb3776256e101a8c21a9b86
Naheun Alam <naheunpranto258@gmail.com>
2026-06-19 19:51:31 +0600

    Fetching Data in Server Components

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,43 @@
  
+// const getPosts = async () => {
+//     const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     return data.json();
+// }
 
-const PostsPage = () => {
+
+
+// const getPosts = async () => {
+//     try{
+//         const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+//         return data.json();
+//     }
+//     catch(err){
+//         throw new Error ('Failed to fetch posts')
+//     }
+// }
+
+
+const getPosts = async () => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    if(!data.ok){
+        throw new Error('Failed to fetch posts')
+    }
+
+    return data.json();
+}
+
+
+const PostsPage = async () => {
+    // const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    // const posts = await data.json();
+
+    const posts = await getPosts();
+
+
     return (
         <div>
-            <h2>Posts are coming soon!!!!!</h2>
+            <h2>Posts are coming soon: {posts.length}</h2>
         </div>
     );
 };
